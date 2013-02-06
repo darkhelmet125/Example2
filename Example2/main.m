db3b3a36da09178f4ca4fb1aec003ef5880fc811
Matthew Short <darkhelmet125@yahoo.com>
2013-02-05 16:02:27 -0800

    Initial Commit

---
+++ Example2/main.m
@@ -0,0 +1,22 @@
+//
+//  main.m
+//  Example2
+//
+//  Created by William Short on 2/5/13.
+//  Copyright (c) 2013 William Short. All rights reserved.
+//
+
+#import <Foundation/Foundation.h>
+
+int main(int argc, const char * argv[])
+{
+
+    @autoreleasepool {
+        
+        // insert code here...
+        NSLog(@"Hello, World!");
+        
+    }
+    return 0;
+}
+
